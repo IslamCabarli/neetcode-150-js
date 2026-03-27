7e8f19d6dfd05c497434ad234aad60d0231dfc84
IslamCabarli <icabarli@std.beu.edu.az>
2026-03-27 17:15:11 +0400

    feat: add solution for container with most water (two pointers)

---
+++ two-pointers/container-with-most-water.js
@@ -0,0 +1,38 @@
+/**
+ * Problem: Container With Most Water
+ * Link: https://leetcode.com/problems/container-with-most-water/
+ * Difficulty: Medium
+ *
+ * Approach:
+ * - Use two pointers (left and right) starting from both ends
+ * - Calculate the area using the minimum height and distance between pointers
+ * - Move the pointer pointing to the shorter height inward
+ *   to potentially find a larger area
+ *
+ * Time Complexity: O(n)
+ * Space Complexity: O(1)
+ */
+
+class Solution {
+    /**
+     * @param {number[]} heights
+     * @return {number}
+     */
+    maxArea(heights) {
+        let left = 0;
+        let right = heights.length - 1;
+        let maxArea = 0;
+        while (left < right)        {
+            let width = right - left;
+            let height = Math.min(heights[left], heights[right]);
+            let area = width * height;
+            maxArea = Math.max(maxArea, area);
+            if (heights[left] < heights[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return maxArea;
+    }
+}
