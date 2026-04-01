3349e4e6ca35a0486791281cf0c1ea5f595fe099
IslamCabarli <icabarli@std.beu.edu.az>
2026-04-02 00:00:02 +0400

    fix comment line

--- stack/valid-parentheses.js
+++ stack/valid-parentheses.js
@@ -6,13 +6,13 @@ An input string is valid if:
 * Open brackets must be closed by the same type of brackets.
 * Open brackets must be closed in the correct order.
 * Every close bracket has a corresponding open bracket of the same type.
-
+*/
 
 class Solution {
     /**
      * @param {string} s
      * @return {boolean}
-     */
+     **/
     isValid(s) {
         while (s.includes('()') || s.includes('{}') || s.includes('[]')) {
             s = s.replace('()', '');
